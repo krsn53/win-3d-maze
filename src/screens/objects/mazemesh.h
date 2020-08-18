56f8a2f321872d61057bf83a57c15e8e552069a7
Takaya Kurosaki <vilizilesatilu@gmail.com>
2020-08-18 18:59:15 +0900

    rm sources

--- src/screens/objects/mazemesh.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "./meshcommon.h"
-#include "./mazemap.h"
-
-Mesh genMeshWall(Texture2D atras, const MazeMap& map, Rectangle texCoord, Rectangle picCoord);
-Mesh genMeshFloor(Texture2D atras, int resX, int resZ, Rectangle down, Rectangle up);
