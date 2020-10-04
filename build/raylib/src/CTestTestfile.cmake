daa42f064d0784e0484ad77700d1ea23807199d7
Takaya Kurosaki <vilizilesatilu@gmail.com>
2020-10-05 02:00:04 +0900

    blend issue

--- build/raylib/src/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/krsn53/programming/c/raylib/win-3d-maze/raylib/src
-# Build directory: /home/krsn53/programming/c/raylib/win-3d-maze/build/raylib/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(pkg-config--static "/home/krsn53/programming/c/raylib/win-3d-maze/raylib/src/../cmake/test-pkgconfig.sh" "--static")
-set_tests_properties(pkg-config--static PROPERTIES  _BACKTRACE_TRIPLES "/home/krsn53/programming/c/raylib/win-3d-maze/raylib/src/CMakeLists.txt;212;add_test;/home/krsn53/programming/c/raylib/win-3d-maze/raylib/src/CMakeLists.txt;0;")
